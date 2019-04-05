8e91f62d56c05f0b3f943b9a6917929f2893699b
Charles Smart <Charlie@Charless-MacBook-Pro.local>
2019-04-04 21:47:41 -0400

    Update icons, fix page change issue

--- src/content.js
+++ src/content.js
@@ -1,40 +1,49 @@
 const observerConfig = {
   attributes: false,
-  subtree: false,
-  childList: true, // Watch for new tweets
+  subtree: true,
+  childList: true,
 }
 
 const observer = new MutationObserver(handleNewTweets);
 const searchURLPrefix = 'https://twitter.com/search?q=';
 const twitterURLPrefix = 'https://twitter.com'
 
-// Grab the stream to observe
-const $stream = document.getElementsByClassName('stream-items')[0];
-
-// Observe it
-observer.observe($stream, observerConfig);
+window.onpopstate = init;
 
 // Handle the tweets that come pre-loaded
 function init() {
-  let $tweets = document.getElementsByClassName('js-stream-tweet');
+  // Grab the stream to observe
+  let $stream = document.getElementById('page-container');
+
+  // Observe it
+  observer.observe(document.body, observerConfig);
+
+  let $tweets = document.querySelectorAll('.js-stream-tweet:not(.quote-added)');
   [].forEach.call($tweets, addButtonToTweet);
 }
 
 // Handle tweets as they're added to the stream
 function handleNewTweets(mutations) {
   mutations.forEach(mutation => {
+    //console.log(mutation);
     // For each added node (a node can include a thread of tweets),
     mutation.addedNodes.forEach($node => {
       // Grab the tweets
-      let $tweets = $node.getElementsByClassName('js-stream-tweet');
-      // Add the buttons
-      [].forEach.call($tweets, addButtonToTweet);
+      // console.log($node);
+      if ($node.nodeType === Node.ELEMENT_NODE) {
+        let $tweets = $node.querySelectorAll('.js-stream-tweet:not(.quote-added)');
+        // Add the buttons
+        [].forEach.call($tweets, addButtonToTweet);
+      }
     })
   });
 }
 
 // Take a tweet, grab its permalink url, generate a search query, add a button
 function addButtonToTweet($tweet) {
+  // Add a class to make sure we don't add a second button
+  $tweet.classList.add('quote-added');
+
   let permalink = $tweet.dataset.permalinkPath;
 
   let $actions = $tweet.getElementsByClassName('js-actions');
